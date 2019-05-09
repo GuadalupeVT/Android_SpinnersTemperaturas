b28e05b8607bd9edb06d4058eea2c9fab89ec1f9
Guadalupe Vazquez <guvadlt@gmail.com>
2019-05-08 23:02:59 -0500

    Se crearon las variables de los componentes

--- app/src/main/java/com/example/sesion04_actividad5/MainActivity.java
+++ app/src/main/java/com/example/sesion04_actividad5/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.sesion04_actividad5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    Spinner spinnerEntrada,spinnerSalida;
+    EditText entrada,salida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerEntrada=findViewById(R.id.spinnerEntrada);
+        spinnerSalida=findViewById(R.id.spinnerSalida);
+        entrada=findViewById(R.id.cajaAConvertir);
+        salida=findViewById(R.id.cajaConvertido);
+
+        spinnerEntrada.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent){
+
     }
 }
